35260918bfb6618c24f7bd3fec21cc1909ab5c90
zlot <zlot@netc.eu>
2020-08-11 22:01:21 +0200

    tuner command

--- zoom-multistomp-patch-changer.ino
+++ zoom-multistomp-patch-changer.ino
@@ -24,7 +24,7 @@
 #define LONGPRESS_THRESHOLD_MS      (1000)
 
 // Delay (in milliseconds) between two patch increments.
-#define AUTOCYCLE_DELAY_MS          (300)
+#define AUTOCYCLE_DELAY_MS          (500)
 
 
 ZoomMS * gZoom;
